102e1854240927e7a42406f8f96e0da718350346
ashiku2 <ashikur1602@gmail.com>
2024-09-19 08:55:59 +0600

    some change

---
+++ src/app/%5lib/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      You can't view this on browser
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="container">
+      Dashboard
+    </div>
+  )
+}
+
+const page = () => {
+  return (
+    <div>
+      Hello world
+      <Dashboard />
+    </div>
+  );
+};
+
+export default page;
